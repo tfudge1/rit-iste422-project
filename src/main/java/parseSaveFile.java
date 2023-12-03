a331e47112b8f8d6de9b319d767ff970c279610a
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2023-12-02 19:39:22 -0500

    removed println(s)

--- src/main/java/parseSaveFile.java
+++ src/main/java/parseSaveFile.java
@@ -18,7 +18,6 @@ public class parseSaveFile extends EdgeConvertFileParser {
             //test for what kind of file we have
             try {
                 currentLine = br.readLine();
-                System.out.println("First line of the file: " + currentLine);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -30,7 +29,6 @@ public class parseSaveFile extends EdgeConvertFileParser {
     
     // getter for the important info
     public EdgeTable[] getEdgeTables() {
-        System.out.println("returning EdgeTables :)");
         if (alTables != null) {
             return alTables.toArray(new EdgeTable[0]);
         }
@@ -38,7 +36,6 @@ public class parseSaveFile extends EdgeConvertFileParser {
     }
     
     public EdgeField[] getEdgeFields() {
-        System.out.println("returning EdgeFields");
         if (alFields != null) {
             return alFields.toArray(new EdgeField[0]);
         }
@@ -52,20 +49,16 @@ public class parseSaveFile extends EdgeConvertFileParser {
     // }
     //where the magic happens :)
     public void parseFile(){ //this method is unclear and confusing in places
-        System.out.println("Begining Parse File of save file");
         try{
             StringTokenizer stTables, stNatFields, stRelFields, stField;
             EdgeTable tempTable;
             EdgeField tempField;
             currentLine = br.readLine();
             currentLine = br.readLine(); //this should be "Table: "
-            System.out.println("entering table while");
             while (currentLine.startsWith("Table: ")) {
                 numFigure = Integer.parseInt(currentLine.substring(currentLine.indexOf(" ") + 1)); //get the Table number
                 currentLine = br.readLine(); //this should be "{"
-                System.out.println("{ = " + currentLine);
                 currentLine = br.readLine(); //this should be "TableName"
-                System.out.println("TableName = " + currentLine);
                 tableName = currentLine.substring(currentLine.indexOf(" ") + 1);
                 tempTable = new EdgeTable(numFigure + DELIM + tableName);
                 //
@@ -97,14 +90,11 @@ public class parseSaveFile extends EdgeConvertFileParser {
                 for (int i = 0; i < numFields; i++) {
                     tempTable.setRelatedField(i, Integer.parseInt(stRelFields.nextToken()));
                 }
-                System.out.println("tempTable = " + tempTable);
                 alTables.add(tempTable);
                 currentLine = br.readLine(); //this should be "}"
                 currentLine = br.readLine(); //this should be "\n"
                 currentLine = br.readLine(); //this should be either the next "Table: ", #Fields#
-                System.out.println("exiting table while");
             }
-            System.out.println("Entering Field while");
             while ((currentLine = br.readLine()) != null) {
                 stField = new StringTokenizer(currentLine, DELIM);
                 numFigure = Integer.parseInt(stField.nextToken());
@@ -121,14 +111,11 @@ public class parseSaveFile extends EdgeConvertFileParser {
                     tempField.setDefaultValue(stField.nextToken());
                 }
                 alFields.add(tempField);
-                System.out.println("Exiting Field while");
             }
             //close up shop
-            System.out.println("finished parsing save file");
             br.close();
             // this.makeArrays();
         }catch(Exception IOException){
-            System.out.println("cought!!!");
             System.out.println(IOException);
             //TODO
         }//catch
